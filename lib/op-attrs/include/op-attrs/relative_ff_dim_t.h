e56aeb7a7369d403a9e25045f4ae5e3e634d5ecb
Victor Li <vli42@sapling2.stanford.edu>
2024-11-14 19:34:57 -0800

    Changed ff_dim_t to use nonnegative_int, added relative_ff_dim_t that uses int

--- lib/op-attrs/include/op-attrs/relative_ff_dim_t.h
+++ lib/op-attrs/include/op-attrs/relative_ff_dim_t.h
@@ -1,13 +1,18 @@
 #ifndef _FLEXFLOW_LIB_OP_ATTRS_INCLUDE_OP_ATTRS_RELATIVE_FF_DIM_T_H
 #define _FLEXFLOW_LIB_OP_ATTRS_INCLUDE_OP_ATTRS_RELATIVE_FF_DIM_T_H
 
+#include "op-attrs/ff_dim_t.dtg.h"
 #include "op-attrs/relative_ff_dim_t.dtg.h"
 #include "rapidcheck.h"
 
+namespace FlexFlow {
+ff_dim_t relative_ff_dim_t_to_ff_dim_t(relative_ff_dim_t ff_dim, int input_dim);
+} // namespace FlexFlow
+
 namespace rc {
 template <>
-struct Arbitrary<FlexFlow::relative_ff_dim_t> {
-  static Gen<FlexFlow::relative_ff_dim_t> arbitrary();
+struct Arbitrary<::FlexFlow::relative_ff_dim_t> {
+  static Gen<::FlexFlow::relative_ff_dim_t> arbitrary();
 };
 } // namespace rc
 
