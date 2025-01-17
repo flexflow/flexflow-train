b01339d6db705293256f1de787c90713413ef8d7
Colin Unger <lockshaw@lockshaw.net>
2025-01-16 19:39:50 -0800

    Update with merged proj commit

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     proj-repo = {
-      url = "github:chenzhuofu/proj";
+      url = "github:lockshaw/proj";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
