f3bf0d77e66f68d7555c1a003f063b6f653db1bb
Marsella8 <45826022+Marsella8@users.noreply.github.com>
2026-01-08 01:42:13 -0800

    Balanced sp decomposition binary tree (#1604)
    * added balanced_binary_sp_tree utilities
    
    * fmt
    
    * fmt
    
    * import fix
    
    * fmt
    
    * fix
    
    * PR fixes
    
    * Format
    
    ---------
    
    Co-authored-by: Pietro Max Marsella <marsella@stanford.edu>
    Co-authored-by: Colin Unger <lockshaw@lockshaw.net>

---
+++ lib/compiler/src/compiler/series_parallel/pcg/get_pcg_balanced_binary_sp_decomposition.cc
@@ -0,0 +1,24 @@
+#include "compiler/series_parallel/pcg/get_pcg_balanced_binary_sp_decomposition.h"
+#include "compiler/series_parallel/pcg/get_pcg_series_parallel_decomposition.h"
+#include "compiler/series_parallel/pcg/pcg_binary_sp_decomposition.h"
+#include "utils/graph/series_parallel/binary_sp_decomposition_tree/balanced_binary_sp_tree_from_nary.h"
+#include <optional>
+
+namespace FlexFlow {
+
+std::optional<PCGBinarySPDecomposition>
+    get_pcg_balanced_binary_sp_decomposition(
+        ParallelComputationGraph const &pcg) {
+
+  std::optional<SeriesParallelDecomposition> spd =
+      get_pcg_series_parallel_decomposition(pcg);
+
+  if (!spd.has_value()) {
+    return std::nullopt;
+  }
+
+  return pcg_binary_sp_decomposition_from_binary_sp_tree(
+      balanced_binary_sp_tree_from_nary(spd.value()));
+}
+
+} // namespace FlexFlow
---
+++ lib/utils/test/src/utils/graph/series_parallel/binary_sp_decomposition_tree/binary_sp_decomposition_tree.cc
@@ -0,0 +1,73 @@
+#include "utils/graph/series_parallel/binary_sp_decomposition_tree/binary_sp_decomposition_tree.h"
+#include <doctest/doctest.h>
+
+using namespace ::FlexFlow;
+
+TEST_SUITE(FF_TEST_SUITE) {
+  TEST_CASE("get_tree_height") {
+    auto make_series_split = [](BinarySPDecompositionTree const &lhs,
+                                BinarySPDecompositionTree const &rhs) {
+      return BinarySPDecompositionTree{BinarySeriesSplit{lhs, rhs}};
+    };
+
+    auto make_parallel_split = [](BinarySPDecompositionTree const &lhs,
+                                  BinarySPDecompositionTree const &rhs) {
+      return BinarySPDecompositionTree{BinaryParallelSplit{lhs, rhs}};
+    };
+
+    auto make_leaf = [](int n) { return BinarySPDecompositionTree{Node{n}}; };
+
+    SUBCASE("leaf only") {
+      BinarySPDecompositionTree input = make_leaf(5);
+
+      nonnegative_int result = get_tree_height(input);
+      nonnegative_int correct = 0_n;
+
+      CHECK(result == correct);
+    }
+
+    SUBCASE("series split") {
+      BinarySPDecompositionTree input = make_series_split(
+          make_series_split(make_series_split(make_leaf(0), make_leaf(1)),
+                            make_series_split(make_leaf(2), make_leaf(3))),
+          make_series_split(make_leaf(4), make_leaf(5)));
+
+      nonnegative_int result = get_tree_height(input);
+      nonnegative_int correct = 3_n;
+
+      CHECK(result == correct);
+    }
+
+    SUBCASE("parallel split") {
+      BinarySPDecompositionTree input = make_parallel_split(
+          make_leaf(4),
+          make_parallel_split(
+              make_parallel_split(make_leaf(3), make_leaf(1)),
+              make_parallel_split(
+                  make_parallel_split(make_leaf(2), make_leaf(3)),
+                  make_leaf(3))));
+
+      nonnegative_int result = get_tree_height(input);
+      nonnegative_int correct = 4_n;
+
+      CHECK(result == correct);
+    }
+
+    SUBCASE("mixed") {
+      BinarySPDecompositionTree input = make_parallel_split(
+          make_leaf(4),
+          make_parallel_split(
+              make_series_split(make_leaf(3), make_leaf(1)),
+              make_parallel_split(
+                  make_series_split(
+                      make_leaf(2),
+                      make_series_split(make_leaf(4), make_leaf(3))),
+                  make_leaf(3))));
+
+      nonnegative_int result = get_tree_height(input);
+      nonnegative_int correct = 5_n;
+
+      CHECK(result == correct);
+    }
+  }
+}
