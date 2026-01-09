9cbf77900be02f43c96b78220ce8c16a96f00989
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-01-09 12:21:30 -0800

    Basic flow to create computation graph instance: computation graph to equivalent dataflow, expand.

--- lib/local-execution/src/local-execution/computation_graph_instance/computation_graph_instance.cc
+++ lib/local-execution/src/local-execution/computation_graph_instance/computation_graph_instance.cc
@@ -1,14 +1,15 @@
 #include "local-execution/computation_graph_instance/computation_graph_instance.h"
-#include "task-spec/dynamic_graph/dynamic_open_dataflow_graph.h"
+#include "task-spec/dynamic_graph/dynamic_open_dataflow_graph_from_cg.h"
+#include "task-spec/dynamic_graph/pass_expansion.h"
+#include "task-spec/dynamic_graph/shard_expansion.h"
+#include "task-spec/dynamic_graph/update_insertion.h"
 #include "utils/exception.h"
 
 namespace FlexFlow {
 
 ComputationGraphInstance::ComputationGraphInstance(
     DynamicOpenDataflowGraph const &graph, LocalTaskRegistry const &registry)
-    : expanded_dataflow_graph(graph), task_registry(registry) {
-  NOT_IMPLEMENTED();
-}
+    : expanded_dataflow_graph(graph), task_registry(registry) {}
 
 ComputationGraphInstance create_computation_graph_instance(
     ComputationGraph const &cg,
@@ -16,7 +17,10 @@ ComputationGraphInstance create_computation_graph_instance(
            std::variant<GenericTensorAccessorW, GenericTensorAccessorR>> const
         &tensors,
     LocalTaskRegistry registry) {
-  DynamicOpenDataflowGraph dg{make_empty_dynamic_open_dataflow_graph()};
+  DynamicOpenDataflowGraph dg = make_dynamic_open_dataflow_graph_from_cg(cg);
+  dg = perform_pass_expansion(dg);
+  dg = perform_shard_expansion(dg);
+  // dg = perform_update_insertion(dg);
   return ComputationGraphInstance{dg, registry};
 }
 
