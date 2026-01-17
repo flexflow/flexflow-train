ad2899393170cb81a29895346447db9b7b8f2fc8
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-01-16 17:01:28 -0800

    Implement task IDs.

--- lib/local-execution/src/local-execution/local_task_registry.cc
+++ lib/local-execution/src/local-execution/local_task_registry.cc
@@ -1,30 +1,150 @@
 #include "local-execution/local_task_registry.h"
 #include "pcg/computation_graph.h"
+#include "task-spec/ops/impl/attention.h"
+#include "task-spec/ops/impl/batch_matmul.h"
+#include "task-spec/ops/impl/batch_norm.h"
+#include "task-spec/ops/impl/broadcast.h"
+#include "task-spec/ops/impl/cast.h"
+#include "task-spec/ops/impl/concat.h"
+#include "task-spec/ops/impl/conv_2d.h"
+#include "task-spec/ops/impl/dropout.h"
+#include "task-spec/ops/impl/element_binary.h"
+#include "task-spec/ops/impl/element_unary.h"
+#include "task-spec/ops/impl/embedding.h"
+#include "task-spec/ops/impl/flat.h"
+#include "task-spec/ops/impl/gather.h"
+#include "task-spec/ops/impl/layer_norm.h"
+#include "task-spec/ops/impl/linear.h"
+#include "task-spec/ops/impl/pool_2d.h"
+#include "task-spec/ops/impl/reduce.h"
+#include "task-spec/ops/impl/reshape.h"
+#include "task-spec/ops/impl/reverse.h"
+#include "task-spec/ops/impl/softmax.h"
+#include "task-spec/ops/impl/split.h"
+#include "task-spec/ops/impl/topk.h"
+#include "task-spec/ops/impl/transpose.h"
 #include "task-spec/task_impl_function.dtg.h"
-#include "utils/containers/contains_key.h"
-#include "utils/containers/filtrans.h"
-#include "utils/containers/flatmap.h"
-#include "utils/containers/generate_map.h"
-#include "utils/containers/map_values.h"
-#include "utils/containers/try_at.h"
-#include "utils/containers/values.h"
-#include "utils/exception.h"
 #include "utils/optional.h"
+#include "utils/overload.h"
 #include <optional>
 
 namespace FlexFlow {
 
 std::optional<TaskImplFunction>
     get_init_task_impl_for_op_attrs(ComputationGraphOpAttrs const &op_attrs) {
-  NOT_IMPLEMENTED();
+
+  return op_attrs.visit<std::optional<TaskImplFunction>>(overload{
+      [](BatchMatmulAttrs const &) { return std::nullopt; },
+      [](BatchNormAttrs const &) { return get_batch_norm_init_task_impl(); },
+      [](BroadcastAttrs const &) { return std::nullopt; },
+      [](CastAttrs const &) { return std::nullopt; },
+      [](ConcatAttrs const &) { return std::nullopt; },
+      [](Conv2DAttrs const &) { return get_conv_2d_init_task_impl(); },
+      [](DropoutAttrs const &) { return get_dropout_init_task_impl(); },
+      [](ElementBinaryAttrs const &) {
+        return get_element_binary_init_task_impl();
+      },
+      [](ElementUnaryAttrs const &) {
+        return get_element_unary_init_task_impl();
+      },
+      [](EmbeddingAttrs const &) { return std::nullopt; },
+      [](FlatAttrs const &) { return std::nullopt; },
+      [](GatherAttrs const &) { return get_gather_init_task_impl(); },
+      [](InputAttrs const &) { return std::nullopt; },
+      [](LayerNormAttrs const &) { return get_layer_norm_init_task_impl(); },
+      [](LinearAttrs const &) { return get_linear_init_task_impl(); },
+      [](MultiHeadAttentionAttrs const &) {
+        return get_attention_init_task_impl();
+      },
+      [](NoopAttrs const &) { return std::nullopt; },
+      [](Pool2DAttrs const &) { return get_pool_2d_init_task_impl(); },
+      [](ReduceAttrs const &) { return get_reduce_init_task_impl(); },
+      [](ReshapeAttrs const &) { return std::nullopt; },
+      [](ReverseAttrs const &) { return std::nullopt; },
+      [](SoftmaxAttrs const &) { return get_softmax_init_task_impl(); },
+      [](SplitAttrs const &) { return std::nullopt; },
+      [](TopKAttrs const &) { return std::nullopt; },
+      [](TransposeAttrs const &) { return std::nullopt; },
+      [](WeightAttrs const &) { return std::nullopt; },
+  });
 }
-TaskImplFunction
+
+std::optional<TaskImplFunction>
     get_fwd_task_impl_for_op_attrs(ComputationGraphOpAttrs const &op_attrs) {
-  NOT_IMPLEMENTED();
+
+  return op_attrs.visit<std::optional<TaskImplFunction>>(overload{
+      [](BatchMatmulAttrs const &) { return get_batch_matmul_fwd_task_impl(); },
+      [](BatchNormAttrs const &) { return get_batch_norm_fwd_task_impl(); },
+      [](BroadcastAttrs const &) { return get_broadcast_fwd_task_impl(); },
+      [](CastAttrs const &) { return get_cast_fwd_task_impl(); },
+      [](ConcatAttrs const &) { return get_concat_fwd_task_impl(); },
+      [](Conv2DAttrs const &) { return get_conv_2d_fwd_task_impl(); },
+      [](DropoutAttrs const &) { return get_dropout_fwd_task_impl(); },
+      [](ElementBinaryAttrs const &) {
+        return get_element_binary_fwd_task_impl();
+      },
+      [](ElementUnaryAttrs const &) {
+        return get_element_unary_fwd_task_impl();
+      },
+      [](EmbeddingAttrs const &) { return get_embedding_fwd_task_impl(); },
+      [](FlatAttrs const &) { return get_flat_fwd_task_impl(); },
+      [](GatherAttrs const &) { return get_gather_fwd_task_impl(); },
+      [](InputAttrs const &) { return std::nullopt; },
+      [](LayerNormAttrs const &) { return get_layer_norm_fwd_task_impl(); },
+      [](LinearAttrs const &) { return get_linear_fwd_task_impl(); },
+      [](MultiHeadAttentionAttrs const &) {
+        return get_attention_fwd_task_impl();
+      },
+      [](NoopAttrs const &) { return std::nullopt; },
+      [](Pool2DAttrs const &) { return get_pool_2d_fwd_task_impl(); },
+      [](ReduceAttrs const &) { return get_reduce_fwd_task_impl(); },
+      [](ReshapeAttrs const &) { return get_reshape_fwd_task_impl(); },
+      [](ReverseAttrs const &) { return get_reverse_fwd_task_impl(); },
+      [](SoftmaxAttrs const &) { return get_softmax_fwd_task_impl(); },
+      [](SplitAttrs const &) { return get_split_fwd_task_impl(); },
+      [](TopKAttrs const &) { return get_topk_fwd_task_impl(); },
+      [](TransposeAttrs const &) { return get_transpose_fwd_task_impl(); },
+      [](WeightAttrs const &) { return std::nullopt; },
+  });
 }
-TaskImplFunction
+
+std::optional<TaskImplFunction>
     get_bwd_task_impl_for_op_attrs(ComputationGraphOpAttrs const &op_attrs) {
-  NOT_IMPLEMENTED();
+
+  return op_attrs.visit<std::optional<TaskImplFunction>>(overload{
+      [](BatchMatmulAttrs const &) { return get_batch_matmul_bwd_task_impl(); },
+      [](BatchNormAttrs const &) { return get_batch_norm_bwd_task_impl(); },
+      [](BroadcastAttrs const &) { return get_broadcast_bwd_task_impl(); },
+      [](CastAttrs const &) { return get_cast_bwd_task_impl(); },
+      [](ConcatAttrs const &) { return get_concat_bwd_task_impl(); },
+      [](Conv2DAttrs const &) { return get_conv_2d_bwd_task_impl(); },
+      [](DropoutAttrs const &) { return get_dropout_bwd_task_impl(); },
+      [](ElementBinaryAttrs const &) {
+        return get_element_binary_bwd_task_impl();
+      },
+      [](ElementUnaryAttrs const &) {
+        return get_element_unary_bwd_task_impl();
+      },
+      [](EmbeddingAttrs const &) { return get_embedding_bwd_task_impl(); },
+      [](FlatAttrs const &) { return get_flat_bwd_task_impl(); },
+      [](GatherAttrs const &) { return get_gather_bwd_task_impl(); },
+      [](InputAttrs const &) { return std::nullopt; },
+      [](LayerNormAttrs const &) { return get_layer_norm_bwd_task_impl(); },
+      [](LinearAttrs const &) { return get_linear_bwd_task_impl(); },
+      [](MultiHeadAttentionAttrs const &) {
+        return get_attention_bwd_task_impl();
+      },
+      [](NoopAttrs const &) { return std::nullopt; },
+      [](Pool2DAttrs const &) { return get_pool_2d_bwd_task_impl(); },
+      [](ReduceAttrs const &) { return get_reduce_bwd_task_impl(); },
+      [](ReshapeAttrs const &) { return get_reshape_bwd_task_impl(); },
+      [](ReverseAttrs const &) { return get_reverse_bwd_task_impl(); },
+      [](SoftmaxAttrs const &) { return get_softmax_bwd_task_impl(); },
+      [](SplitAttrs const &) { return get_split_bwd_task_impl(); },
+      [](TopKAttrs const &) { return get_topk_bwd_task_impl(); },
+      [](TransposeAttrs const &) { return get_transpose_bwd_task_impl(); },
+      [](WeightAttrs const &) { return std::nullopt; },
+  });
 }
 
 std::optional<DeviceSpecificPerDeviceOpState>
@@ -47,9 +167,13 @@ std::optional<DeviceSpecificPerDeviceOpState>
 std::optional<milliseconds_t>
     call_fwb_task_impl(ComputationGraphOpAttrs const &op_attrs,
                        TaskArgumentAccessor const &acc) {
-
-  TaskImplFunction task_impl_fn = get_fwd_task_impl_for_op_attrs(op_attrs);
-  auto fn = task_impl_fn.get<FwdBwdOpTaskImplFunction>().function_ptr;
+  std::optional<TaskImplFunction> task_impl_fn =
+      get_fwd_task_impl_for_op_attrs(op_attrs);
+  if (!task_impl_fn) {
+    return std::nullopt;
+  }
+  auto fn =
+      assert_unwrap(task_impl_fn).get<FwdBwdOpTaskImplFunction>().function_ptr;
 
   return fn(acc);
 }
