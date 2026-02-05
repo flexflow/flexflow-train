90b92c789aa5185e16d3c7b70d196fe9091bb006
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-04 16:39:30 -0800

    Add Realm manager and test it.

--- lib/realm-execution/test/src/realm-execution/test_e2e.cc
+++ lib/realm-execution/test/src/realm-execution/test_e2e.cc
@@ -1,8 +1,16 @@
 #include "realm-execution/parallel_computation_graph_instance/parallel_computation_graph_instance.h"
+#include "realm-execution/realm_manager.h"
 #include <doctest/doctest.h>
 
 using namespace ::FlexFlow;
 
 TEST_SUITE(FF_TEST_SUITE) {
-  TEST_CASE("RealmBackend e2e Training") {}
+  TEST_CASE("RealmBackend e2e Training") {
+    char fake_executable_name[] = "fake_executable_name";
+    std::vector<char *> fake_args{fake_executable_name};
+    int fake_argc = fake_args.size();
+    char **fake_argv = fake_args.data();
+    RealmManager manager(&fake_argc, &fake_argv);
+    manager.shutdown();
+  }
 }
