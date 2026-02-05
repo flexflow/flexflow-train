32c66578a2dc23249feaac5e9845f15fb4740788
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-05 14:34:22 -0800

    Avoid pulling in the entire invocation.

--- lib/realm-execution/include/realm-execution/task_id_t.h
+++ lib/realm-execution/include/realm-execution/task_id_t.h
@@ -4,13 +4,13 @@
 #include "op-attrs/pcg_operator_attrs.dtg.h"
 #include "pcg/optimizer_attrs.dtg.h"
 #include "realm-execution/task_id_t.dtg.h"
-#include "task-spec/dynamic_graph/dynamic_node_invocation.dtg.h"
+#include "task-spec/dynamic_graph/dynamic_node_attrs.dtg.h"
 #include <optional>
 
 namespace FlexFlow {
 
 std::optional<task_id_t>
-    get_task_id_for_op(DynamicNodeInvocation const &,
+    get_task_id_for_op(DynamicNodeAttrs const &,
                        std::optional<OptimizerAttrs> const &);
 
 std::optional<task_id_t>
