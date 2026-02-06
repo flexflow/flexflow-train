05eeada94290694a10c7e7bab2190ccb663a0526
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-05 17:08:38 -0800

    Some work on Realm task registry.

---
+++ lib/realm-execution/include/realm-execution/realm_task_registry.h
@@ -0,0 +1,13 @@
+#ifndef _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_REALM_TASK_REGISTRY_H
+#define _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_REALM_TASK_REGISTRY_H
+
+#include "realm-execution/realm.h"
+#include "realm-execution/task_id_t.dtg.h"
+
+namespace FlexFlow {
+
+Realm::Event register_all_tasks();
+
+} // namespace FlexFlow
+
+#endif
