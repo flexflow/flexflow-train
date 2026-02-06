60989fe4fd9d00b3cc0d4645ec3171282985157b
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-06 13:24:41 -0800

    Refactor.

--- lib/realm-execution/src/realm-execution/realm_context.cc
+++ lib/realm-execution/src/realm-execution/realm_context.cc
@@ -1,5 +1,6 @@
 #include "realm-execution/realm_context.h"
 #include "op-attrs/datatype.h"
+#include "op-attrs/tensor_dims.dtg.h"
 #include "realm-execution/realm_task_id_t.h"
 #include "realm-execution/task_id_t.dtg.h"
 #include "utils/exception.h"
@@ -27,91 +28,87 @@ device_id_t const &RealmContext::get_current_device_idx() const {
   NOT_IMPLEMENTED();
 }
 
+template <int N>
+static Realm::Rect<N> rect_from_dims(TensorDims const &dims) {
+  std::vector<int> values{dims.ff_ordered.begin(), dims.ff_ordered.end()};
+  return Realm::Rect<N>{Realm::Point<N>::ZEROES(),
+                        Realm::Point<N>{values.data()} -
+                            Realm::Point<N>::ONES()};
+}
+
 std::pair<Realm::RegionInstance, Realm::Event>
     RealmContext::create_instance(Realm::Memory memory,
                                   TensorShape const &shape,
                                   Realm::ProfilingRequestSet const &prs,
                                   Realm::Event wait_on) {
-  std::vector<int> dims{shape.dims.ff_ordered.begin(),
-                        shape.dims.ff_ordered.end()};
   std::vector<size_t> field_sizes{
       static_cast<size_t>(int{size_of_datatype(shape.data_type)})};
   Realm::RegionInstance inst;
   Realm::Event ready;
-  switch (dims.size()) {
+  switch (shape.dims.ff_ordered.num_dims()) {
 #if REALM_MAX_DIM >= 1
     case 1:
-      ready = Realm::RegionInstance::create_instance(
-          inst,
-          memory,
-          Realm::Rect<1>{Realm::Point<1>::ZEROES(),
-                         Realm::Point<1>{dims.data()} -
-                             Realm::Point<1>::ONES()},
-          field_sizes,
-          /*block_size=*/0 /*SOA*/,
-          prs,
-          wait_on);
+      ready =
+          Realm::RegionInstance::create_instance(inst,
+                                                 memory,
+                                                 rect_from_dims<1>(shape.dims),
+                                                 field_sizes,
+                                                 0 /*SOA*/,
+                                                 prs,
+                                                 wait_on);
       break;
 #endif
 #if REALM_MAX_DIM >= 2
     case 2:
-      ready = Realm::RegionInstance::create_instance(
-          inst,
-          memory,
-          Realm::Rect<2>{Realm::Point<2>::ZEROES(),
-                         Realm::Point<2>{dims.data()} -
-                             Realm::Point<2>::ONES()},
-          field_sizes,
-          /*block_size=*/0 /*SOA*/,
-          prs,
-          wait_on);
+      ready =
+          Realm::RegionInstance::create_instance(inst,
+                                                 memory,
+                                                 rect_from_dims<2>(shape.dims),
+                                                 field_sizes,
+                                                 0 /*SOA*/,
+                                                 prs,
+                                                 wait_on);
       break;
 #endif
 #if REALM_MAX_DIM >= 3
     case 3:
-      ready = Realm::RegionInstance::create_instance(
-          inst,
-          memory,
-          Realm::Rect<3>{Realm::Point<3>::ZEROES(),
-                         Realm::Point<3>{dims.data()} -
-                             Realm::Point<3>::ONES()},
-          field_sizes,
-          /*block_size=*/0 /*SOA*/,
-          prs,
-          wait_on);
+      ready =
+          Realm::RegionInstance::create_instance(inst,
+                                                 memory,
+                                                 rect_from_dims<3>(shape.dims),
+                                                 field_sizes,
+                                                 0 /*SOA*/,
+                                                 prs,
+                                                 wait_on);
       break;
 #endif
 #if REALM_MAX_DIM >= 4
     case 4:
-      ready = Realm::RegionInstance::create_instance(
-          inst,
-          memory,
-          Realm::Rect<4>{Realm::Point<4>::ZEROES(),
-                         Realm::Point<4>{dims.data()} -
-                             Realm::Point<4>::ONES()},
-          field_sizes,
-          /*block_size=*/0 /*SOA*/,
-          prs,
-          wait_on);
+      ready =
+          Realm::RegionInstance::create_instance(inst,
+                                                 memory,
+                                                 rect_from_dims<4>(shape.dims),
+                                                 field_sizes,
+                                                 0 /*SOA*/,
+                                                 prs,
+                                                 wait_on);
       break;
 #endif
 #if REALM_MAX_DIM >= 5
     case 5:
-      ready = Realm::RegionInstance::create_instance(
-          inst,
-          memory,
-          Realm::Rect<5>{Realm::Point<5>::ZEROES(),
-                         Realm::Point<5>{dims.data()} -
-                             Realm::Point<5>::ONES()},
-          field_sizes,
-          /*block_size=*/0 /*SOA*/,
-          prs,
-          wait_on);
+      ready =
+          Realm::RegionInstance::create_instance(inst,
+                                                 memory,
+                                                 rect_from_dims<5>(shape.dims),
+                                                 field_sizes,
+                                                 0 /*SOA*/,
+                                                 prs,
+                                                 wait_on);
       break;
 #endif
     default:
       PANIC("TensorShape dims greater than REALM_MAX_DIM",
-            fmt::to_string(dims.size()));
+            fmt::to_string(shape.dims.ff_ordered.num_dims()));
       break;
   }
   this->outstanding_events.push_back(ready);
