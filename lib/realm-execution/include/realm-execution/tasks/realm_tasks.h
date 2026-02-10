ea2b652cd0d851efecf0ae962f0029daab690314
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-10 10:47:41 -0800

    Make use of task args struct.

--- lib/realm-execution/include/realm-execution/tasks/realm_tasks.h
+++ lib/realm-execution/include/realm-execution/tasks/realm_tasks.h
@@ -2,10 +2,30 @@
 #define _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_REALM_TASKS_H
 
 #include "realm-execution/realm.h"
+#include "realm-execution/realm_context.h"
+#include "task-spec/dynamic_graph/dynamic_node_invocation.dtg.h"
+#include <type_traits>
 
 namespace FlexFlow {
 
 void op_task_body(void const *, size_t, void const *, size_t, Realm::Processor);
+
+// TODO: at some point we're going to have to actually serialize these, but for
+// now just pass the pointer and assume we're running inside a single address
+// space
+struct DeviceInitTaskArgs {
+public:
+  DynamicNodeInvocation *invocation;
+};
+static_assert(std::has_unique_object_representations_v<DeviceInitTaskArgs>);
+
+void device_init_task_body(
+    void const *, size_t, void const *, size_t, Realm::Processor);
+
+struct ControllerTaskArgs {
+public:
+  std::function<void(RealmContext &)> thunk;
+};
 
 void controller_task_body(
     void const *, size_t, void const *, size_t, Realm::Processor);
--- lib/realm-execution/src/realm-execution/tasks/realm_tasks.cc
+++ lib/realm-execution/src/realm-execution/tasks/realm_tasks.cc
@@ -1,9 +1,15 @@
 #include "realm-execution/tasks/realm_tasks.h"
 #include "realm-execution/realm_context.h"
+#include "utils/exception.h"
 
 namespace FlexFlow {
 
 void op_task_body(
+    void const *, size_t, void const *, size_t, Realm::Processor) {
+  NOT_IMPLEMENTED();
+}
+
+void device_init_task_body(
     void const *, size_t, void const *, size_t, Realm::Processor) {
   NOT_IMPLEMENTED();
 }
@@ -13,12 +19,12 @@ void controller_task_body(void const *args,
                           void const *userdata,
                           size_t userlen,
                           Realm::Processor proc) {
-  ASSERT(arglen == sizeof(std::function<void(RealmContext &)>));
-  std::function<void(RealmContext &)> thunk =
-      *reinterpret_cast<std::function<void(RealmContext &)> const *>(args);
+  ASSERT(arglen == sizeof(ControllerTaskArgs));
+  ControllerTaskArgs task_args =
+      *reinterpret_cast<ControllerTaskArgs const *>(args);
 
   RealmContext ctx{proc};
-  thunk(ctx);
+  task_args.thunk(ctx);
 }
 
 } // namespace FlexFlow
