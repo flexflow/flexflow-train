0535c34253805f0f3e940c0543e9c8781ccee975
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-10 14:26:45 -0800

    Refactor task APIs.

---
+++ lib/realm-execution/include/realm-execution/tasks/impl/device_init_return_task.h
@@ -0,0 +1,21 @@
+#ifndef _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_TASKS_IMPL_DEVICE_INIT_RETURN_TASK_H
+#define _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_TASKS_IMPL_DEVICE_INIT_RETURN_TASK_H
+
+#include "realm-execution/realm.h"
+#include "realm-execution/realm_context.h"
+#include "task-spec/device_specific_per_device_op_state.dtg.h"
+
+namespace FlexFlow {
+
+void device_init_return_task_body(
+    void const *, size_t, void const *, size_t, Realm::Processor);
+
+Realm::Event spawn_device_init_return_task(
+    RealmContext &ctx,
+    Realm::Processor origin_proc,
+    DeviceSpecificPerDeviceOpState const &result,
+    DeviceSpecificPerDeviceOpState *origin_result_ptr);
+
+} // namespace FlexFlow
+
+#endif
