2fb0b34ad4f18d14284f0df27e9954b2cbb5c456
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-13 10:30:17 -0800

    Switch to own DeviceSpecific implementation with raw pointers.

--- lib/realm-execution/include/realm-execution/distributed_device_handle.h
+++ lib/realm-execution/include/realm-execution/distributed_device_handle.h
@@ -1,7 +1,7 @@
 #ifndef _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_DISTRIBUTED_DEVICE_HANDLE_H
 #define _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_DISTRIBUTED_DEVICE_HANDLE_H
 
-#include "realm-execution/device_specific_managed_per_device_ff_handle.dtg.h"
+#include "realm-execution/device_specific_managed_per_device_ff_handle.h"
 #include "realm-execution/hash/processor.h"
 #include "realm-execution/realm.h"
 #include "realm-execution/realm_context.h"
--- lib/realm-execution/src/realm-execution/device_specific_managed_per_device_ff_handle.cc
+++ lib/realm-execution/src/realm-execution/device_specific_managed_per_device_ff_handle.cc
@@ -3,19 +3,27 @@
 
 namespace FlexFlow {
 
+DeviceSpecificManagedPerDeviceFFHandle::DeviceSpecificManagedPerDeviceFFHandle(
+    device_id_t owner, std::optional<ManagedPerDeviceFFHandle *> handle)
+    : owner(owner), handle(handle) {}
+
+std::optional<ManagedPerDeviceFFHandle *>
+    DeviceSpecificManagedPerDeviceFFHandle::get(device_id_t device_idx) const {
+  ASSERT(this->owner == device_idx);
+  return this->handle;
+}
+
 DeviceSpecificManagedPerDeviceFFHandle make_device_specific_managed_handle(
     device_id_t const &device_id,
     std::optional<ManagedPerDeviceFFHandle *> const &managed_handle) {
-  return DeviceSpecificManagedPerDeviceFFHandle{
-      DeviceSpecific<std::optional<ManagedPerDeviceFFHandle *>>::create(
-          device_id, managed_handle)};
+  return DeviceSpecificManagedPerDeviceFFHandle{device_id, managed_handle};
 }
 
 device_handle_t device_handle_t_from_device_specific_managed_handle(
     DeviceSpecificManagedPerDeviceFFHandle const &device_specific,
     device_id_t device_idx) {
   return device_handle_t_from_managed_handle_ptr(
-      *device_specific.handle.get(device_idx));
+      *device_specific.get(device_idx));
 }
 
 } // namespace FlexFlow
