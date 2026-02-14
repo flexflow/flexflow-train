30bddfdfa8fbc6da55975dd4546d752918b50534
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-02-14 12:18:49 -0800

    Most of serializer finished.

---
+++ lib/realm-execution/include/realm-execution/tasks/serializer/task_arg_serializer.h
@@ -0,0 +1,26 @@
+#ifndef _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_TASKS_SERIALIZER_TASK_ARG_SERIALIZER_H
+#define _FLEXFLOW_LIB_REALM_EXECUTION_INCLUDE_REALM_EXECUTION_TASKS_SERIALIZER_TASK_ARG_SERIALIZER_H
+
+#include <nlohmann/json.hpp>
+#include <string>
+#include <string_view>
+
+namespace FlexFlow {
+
+template <typename T>
+std::string serialize_task_args(T const &args) {
+  nlohmann::json j;
+  args.serialize(j);
+  return j.dump();
+}
+
+template <typename T>
+T deserialize_task_args(void const *args, size_t arglen) {
+  nlohmann::json j = nlohmann::json::parse(
+      std::string_view{reinterpret_cast<char const *>(args), arglen});
+  return T::deserialize(j);
+}
+
+} // namespace FlexFlow
+
+#endif
