c62405fbd7906f75cbb04e8659596303fc8f5d06
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-03-03 20:28:18 -0800

    Update deploy.sh script.

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,29 @@ git submodule update --init
 mkdir -p deploy
 pushd deploy
 
+function build_cmake_library {
+    dep_name="$1"
+    dep_url="$2"
+    dep_args=("${@:3}")
+    if [[ ! -e ${dep_name} ]]; then
+        git clone "${dep_url}" "${dep_name}"
+    fi
+    if [[ ! -e "${dep_name}"_install/lib ]]; then
+        mkdir -p "${dep_name}"_build "${dep_name}"_install
+        pushd "${dep_name}"_build
+        cmake ../"${dep_name}" -DCMAKE_INSTALL_PREFIX="$PWD"/../"${dep_name}"_install "${dep_args[@]}"
+        make install -j20
+        popd
+    fi
+    export "${dep_name}"_ROOT="$PWD"/"${dep_name}"_install
+}
+
+if [[ ! -e uv ]]; then
+    mkdir uv
+    XDG_BIN_HOME="$PWD"/uv sh <(curl -LsSf https://astral.sh/uv/install.sh) --no-modify-path
+fi
+export PATH="$PATH:$PWD/uv"
+
 if [[ ! -e gasnet ]]; then
     git clone https://github.com/StanfordLegion/gasnet.git
 fi
@@ -17,70 +40,34 @@ export GASNet_ROOT="$PWD"/gasnet/release
 
 set -x
 
-if [[ ! -e realm ]]; then
-    git clone https://github.com/StanfordLegion/realm.git
-fi
-if [[ ! -e realm_install/lib ]]; then
-    mkdir -p realm_build realm_install
-    pushd realm_build
-    cmake ../realm -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="$PWD"/../realm_install -DBUILD_SHARED_LIBS=ON -DREALM_ENABLE_CUDA=ON -DREALM_ENABLE_PREALM=ON -DREALM_MAX_DIM=5
-    make install -j20
-    popd # realm_build
-fi
-export Realm_ROOT="$PWD"/realm_install
+build_cmake_library Realm https://github.com/StanfordLegion/realm.git -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=ON -DREALM_ENABLE_CUDA=ON -DREALM_ENABLE_PREALM=ON -DREALM_MAX_DIM=5
 
-if [[ ! -e zstd ]]; then
-    git clone https://github.com/facebook/zstd.git
-fi
-if [[ ! -e zstd_install/lib ]]; then
-    mkdir -p zstd_build zstd_install
-    pushd zstd_build
-    cmake ../zstd -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="$PWD"/../zstd_install -DBUILD_SHARED_LIBS=ON
-    make install -j20
-    popd # zstd_build
-fi
-export zstd_ROOT="$PWD"/zstd_install
+build_cmake_library zstd https://github.com/facebook/zstd.git -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
 
-if [[ ! -e benchmark ]]; then
-    git clone https://github.com/google/benchmark.git
-fi
-if [[ ! -e benchmark_install/lib ]]; then
-    mkdir -p benchmark_build benchmark_install
-    pushd benchmark_build
-    cmake ../benchmark -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD"/../benchmark_install -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON
-    make install -j20
-    popd # benchmark_build
-fi
-export benchmark_ROOT="$PWD"/benchmark_install
+build_cmake_library benchmark https://github.com/google/benchmark.git -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON
 
-if [[ ! -e libassert ]]; then
-    git clone https://github.com/jeremy-rifkin/libassert.git
-fi
-if [[ ! -e libassert_install/lib ]]; then
-    mkdir -p libassert_build libassert_install
-    pushd libassert_build
-    cmake ../libassert -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="$PWD"/../libassert_install
-    make install -j20
-    popd # libassert_build
-fi
-export libassert_ROOT="$PWD"/libassert_install
+build_cmake_library libassert https://github.com/jeremy-rifkin/libassert.git -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=ON
 
-if [[ ! -e cpptrace ]]; then
-    git clone https://github.com/jeremy-rifkin/cpptrace.git
-fi
-if [[ ! -e cpptrace_install/lib ]]; then
-    mkdir -p cpptrace_build cpptrace_install
-    pushd cpptrace_build
-    cmake ../cpptrace -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="$PWD"/../cpptrace_install -DBUILD_SHARED_LIBS=ON -DCPPTRACE_USE_EXTERNAL_ZSTD=ON
-    make install -j20
-    popd # cpptrace_build
-fi
-export cpptrace_ROOT="$PWD"/cpptrace_install
+build_cmake_library cpptrace https://github.com/jeremy-rifkin/cpptrace.git -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=ON -DCPPTRACE_USE_EXTERNAL_ZSTD=ON
+
+build_cmake_library NCCL https://github.com/NVIDIA/nccl.git -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+
+# if [[ ! -e proj ]]; then
+#     git clone -b python-install https://github.com/elliottslaughter/proj.git
+#     pushd proj
+#     uv venv
+#     uv sync
+#     popd # proj
+# fi
+# source proj/.venv/bin/activate
+# export PATH="$PATH:$PWD/proj/bin"
+# export PYTHONPATH="$PYTHONPATH:$PWD/proj"
 
 popd # deploy
 
 mkdir build install
 pushd build
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$PWD/../install -DFF_USE_EXTERNAL_GBENCHMARK=ON -DFF_USE_EXTERNAL_LIBASSERT=ON
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$PWD/../install -DFF_USE_EXTERNAL_GBENCHMARK=ON -DFF_USE_EXTERNAL_LIBASSERT=ON -DFF_USE_EXTERNAL_NCCL=ON
+# proj dtgen
 make install -j20
 popd # build
