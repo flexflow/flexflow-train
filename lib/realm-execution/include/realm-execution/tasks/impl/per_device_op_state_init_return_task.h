015ce369f2d38c29675a961948073db7a7f9d151
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-03-12 13:35:18 -0700

    Fix documentation tests.

--- lib/realm-execution/include/realm-execution/tasks/impl/per_device_op_state_init_return_task.h
+++ lib/realm-execution/include/realm-execution/tasks/impl/per_device_op_state_init_return_task.h
@@ -23,10 +23,12 @@ void per_device_op_state_init_return_task_body(
  * \brief Launches the task (\ref per_device_op_state_init_return_task_body) for
  * returning the asynchronously-initialized \ref PerDeviceOpState.
  *
+ * \param ctx The Realm context.
  * \param origin_proc The processor to send the result to.
  * \param result The result value.
  * \param origin_result_ptr The pointer, on the origin processor, to which the
  * result should be written.
+ * \param precondition Event precondition to the resulting task.
  *
  * To understand how this fits into the broader structure of \ref
  * realm-execution, see \ref realm-execution-tasks.
