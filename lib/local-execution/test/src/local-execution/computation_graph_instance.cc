3d3d65ac76a53d6d0da636841c416286c93f59e4
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-03-25 10:59:46 -0700

    Remove FFIterationConfig.

--- lib/local-execution/test/src/local-execution/computation_graph_instance.cc
+++ lib/local-execution/test/src/local-execution/computation_graph_instance.cc
@@ -159,7 +159,6 @@ TEST_SUITE(FF_TEST_SUITE) {
             /*allocator=*/allocator,
             /*profiling_settings=*/ProfilingSettings{0, 0},
             /*device_handle=*/ff_handle,
-            /*iteration_config=*/FFIterationConfig{1_p},
             /*device_idx=*/device_idx);
 
     // begin training loop
@@ -171,7 +170,6 @@ TEST_SUITE(FF_TEST_SUITE) {
           /*instance=*/computation_graph_instance,
           /*profiling_settings=*/ProfilingSettings{0, 0},
           /*ff_handle=*/ff_handle,
-          /*iteration_config=*/FFIterationConfig{1_p},
           /*device_idx=*/device_idx);
       loss_values.push_back(copy_tensor_accessor_r(
           computation_graph_instance.get_loss_tensor_accessor().value(),
@@ -330,7 +328,6 @@ TEST_SUITE(FF_CUDA_TEST_SUITE) {
             /*allocator=*/allocator,
             /*profiling_settings=*/ProfilingSettings{0, 0},
             /*device_handle=*/ff_handle,
-            /*iteration_config=*/FFIterationConfig{1_p},
             /*device_idx=*/device_idx);
 
     // begin training loop
@@ -344,7 +341,6 @@ TEST_SUITE(FF_CUDA_TEST_SUITE) {
           /*instance=*/computation_graph_instance,
           /*profiling_settings=*/ProfilingSettings{0, 0},
           /*ff_handle=*/ff_handle,
-          /*iteration_config=*/FFIterationConfig{1_p},
           /*device_idx=*/device_idx);
       loss_values.push_back(copy_tensor_accessor_r(
           computation_graph_instance.get_loss_tensor_accessor().value(),
