16e5d1a58b1c63fec802cc1d2f6cd02fd6342f5b
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-09-16 11:44:19 -0700

    Fix build under GCC 14 and add GCC 14 deploy test. (#1686)

--- lib/utils/src/utils/containers/inplace_filter.cc
+++ lib/utils/src/utils/containers/inplace_filter.cc
@@ -1,1 +1,29 @@
 #include "utils/containers/inplace_filter.h"
+#include "utils/archetypes/ordered_value_type.h"
+#include "utils/archetypes/value_type.h"
+
+namespace FlexFlow {
+
+using Elem = value_type<0>;
+using O_Elem = ordered_value_type<0>;
+
+template void inplace_filter(std::vector<Elem> &,
+                             std::function<bool(Elem const &)> const &);
+
+template void inplace_filter(std::unordered_set<Elem> &,
+                             std::function<bool(Elem const &)> const &);
+
+template void inplace_filter(std::set<O_Elem> &,
+                             std::function<bool(O_Elem const &)> const &);
+
+using V = value_type<0>;
+
+template void
+    inplace_filter(std::unordered_map<Elem, V> &s,
+                   std::function<bool(std::pair<Elem, V> const &)> const &);
+
+template void
+    inplace_filter(std::map<O_Elem, V> &s,
+                   std::function<bool(std::pair<O_Elem, V> const &)> const &);
+
+} // namespace FlexFlow
