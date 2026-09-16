e1e07ceabe76bf97c505e48092b5ad433450f2db
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-09-16 11:36:02 -0700

    Minor cleanup for task-spec profiling.

--- lib/task-spec/include/task-spec/profiling.h
+++ lib/task-spec/include/task-spec/profiling.h
@@ -2,7 +2,6 @@
 #define _FLEXFLOW_LIB_TASK_SPEC_INCLUDE_TASK_SPEC_PROFILING_H
 
 #include "kernels/profiling.h"
-#include "utils/optional.h"
 #include <spdlog/spdlog.h>
 
 namespace FlexFlow {
@@ -18,7 +17,6 @@ std::optional<milliseconds_t>
     f(get_stream_for_device_type(device_type), std::forward<Ts>(ts)...);
     return std::nullopt;
   } else {
-    ProfilingSettings settings = assert_unwrap(profiling);
     milliseconds_t elapsed = profiling_wrapper<F, Ts...>(
         f, profiling.value(), device_type, std::forward<Ts>(ts)...);
     spdlog::debug(s, elapsed);
