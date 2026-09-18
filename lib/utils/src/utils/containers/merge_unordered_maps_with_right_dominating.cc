0530410985a8ead725fa8a58fab9ad3827ffbc0c
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-09-18 11:55:54 -0700

    Remove template hacks.

--- lib/utils/src/utils/containers/merge_unordered_maps_with_right_dominating.cc
+++ lib/utils/src/utils/containers/merge_unordered_maps_with_right_dominating.cc
@@ -5,8 +5,9 @@ namespace FlexFlow {
 
 using K = value_type<0>;
 using V = value_type<1>;
+using C = std::vector<std::unordered_map<K, V>>;
 
-template std::unordered_map<K, V> merge_unordered_maps_with_right_dominating(
-    std::vector<std::unordered_map<K, V>> const &);
+template std::unordered_map<K, V>
+    merge_unordered_maps_with_right_dominating(C const &);
 
 } // namespace FlexFlow
